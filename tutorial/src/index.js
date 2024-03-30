0e4f3555f4ba8793dfd953778f6dc16d65ce9ffa
Barry Keith Salmon <salmon.barry1@gmail.com>
2024-03-30 17:51:59 +0800

    Continued with React course

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// function Greeting() {
+//   return <h2>My First Component</h2>;
+// }
+// function Greeting() {
+//   return React.createElement("h2", {}, "hello world");
+// }
+function Greeting() {
+  return (
+    <div>
+      <h2>My new element</h2>
+    </div>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<Greeting />);
